4e70b3e35b67c51df808c4ed129540e757f029b8
Tony Ivanov <telamohn@gmail.com>
2018-12-22 16:39:43 +0100

    Ported all code, all tests pass

---
+++ index.js
@@ -0,0 +1,149 @@
+var crypto = require('hypercore-crypto')
+var debug = require('debug')('multifeed-sigrid')
+var SIGNATURES_JSON = 'signatures.json'
+var assert = require('assert')
+/**
+ * key      hexstring or buffer, will only replicate feeds that are signed with
+ *          this key
+ * storage  A random-access storage used for persisting known signatures
+ *          locally
+ * secret   Hexstring or Buffer, enables local writers to be signed for
+ *          replication.
+ */
+function Sigrid(key, storage, secret) {
+  if (!(this instanceof Sigrid)) return new Sigrid(key, storage, secret)
+  assert.ok(key, 'Signature checking key is required')
+  assert.ok(storage, 'Storage is required')
+  this.key = ensureBuffer(key)
+  this._canSign = !!secret
+  this._secret = ensureBuffer(secret)
+  this._store = storage(SIGNATURES_JSON)
+  this._signatures = {}
+}
+Sigrid.prototype.init = function(multifeed) {
+  multifeed.key = this.key  // overwrite multifeed's encryption key,
+                            // might remove this for purposes other than
+                            // personal-feed
+  multifeed.prependListener('feed', this._onFeed.bind(this))
+  this._reload()
+}
+
+Sigrid.prototype.have = function(local, next) {
+  var self = this
+  // only share verified feed-keys
+  var feedKeys = local.keys.filter(function(fkey){
+    return verify(fkey, self._signatures[fkey], self.key)
+  })
+
+  // extract their signatures
+  var sigs = feedKeys.map(function(key) { return self._signatures[key] })
+  next(feedKeys, {signatures: sigs})
+}
+
+Sigrid.prototype.want = function(remote, next) {
+  var self = this
+  var sigsDiscovered = false
+  var verified = remote.keys.filter(function(fkey, i) {
+    var valid = verify(fkey, self._signatures[fkey] || remote.signatures[i], self.key)
+    if (valid && !self._signatures[fkey]) {
+      self._signatures[fkey] = remote.signatures[i]
+      sigsDiscovered = true
+    }
+    return valid
+  })
+  if (sigsDiscovered) self._save(function (err) {
+    if (err) throw err
+    next(verified)
+  })
+  else next(verified)
+}
+
+/**
+ * feed - Hexstring or buffer containing feed's key
+ * signature - Hexstring or buffer containing signature
+ * done(err) - Functoin (optional) called when new signature is successfully stored
+ */
+Sigrid.prototype.setSignature = function(feed, signature, done) {
+  if (typeof done !== 'function') done = function(err) { if(err) throw err }
+  feed = stringKey(feed)
+  signature = stringKey(signature)
+  this._signatures = this._signatures || {}
+  this._signatures[feed] = signature
+  this._save(done)
+}
+
+
+Sigrid.prototype._save = function(done) {
+  if (typeof done !== 'function') done = function(err) { if(err) throw err }
+  this._signatures = this._signatures || {}
+  var self = this
+  var sigBuf = Buffer.from(JSON.stringify(self._signatures))
+  var sizeBuf = Buffer.alloc(4)
+  sizeBuf.writeUInt32LE(sigBuf.length)
+
+  self._store.write(0, sizeBuf, function(err) {
+    if (err) return done(err)
+    self._store.write(4, sigBuf, done)
+  })
+}
+
+Sigrid.prototype._reload = function (done) {
+  if (typeof done !== 'function') done = function(err) { if(err) throw err }
+  var self = this
+  self._signatures = {}
+  self._store.read(0,4,function(err, chunk) {
+    if (err) {
+      debug('Loading signatures failed, this is normal for empty multifeeds', err.message)
+      return done()
+    }
+    var size = chunk.readUInt32LE()
+    self._store.read(4, size, function(err, chunk) {
+      if (err) return done(err)
+      self._signatures = JSON.parse(chunk.toString('utf8'))
+      debug('Signatures reloaded')
+      done(null, self._signatures)
+    })
+  })
+}
+
+Sigrid.prototype._onFeed = function(feed, name) {
+  if(feed.writable) this._createSignature(feed.key)
+}
+
+Sigrid.prototype._createSignature = function  (buffer, done) {
+  if (typeof done !== 'function') done = function(err) { if(err) throw err }
+  if (!this._canSign) return done()
+  buffer = ensureBuffer(buffer)
+  // Sign the feed using provided private key
+  var sig = crypto.sign(buffer, this._secret)
+  debug("Signing new writer", buffer.toString('hex'), sig.toString('hex'))
+
+  // Verify the signature
+  if (!verify(buffer, sig, this.key)) {
+    return done(new Error('Invalid signature produced, have you provided a correct key pair?'))
+  }
+  // Store the signature
+  this.setSignature(buffer, sig, done)
+}
+
+module.exports = Sigrid
+
+function verify (msg, sig, key) {
+  if (!msg || !sig || !key) return false
+  return crypto.verify(
+    ensureBuffer(msg),
+    ensureBuffer(sig),
+    ensureBuffer(key)
+  )
+}
+
+function stringKey(input) {
+  if (Buffer.isBuffer(input)) return input.toString('hex')
+  return input
+}
+
+function ensureBuffer(input) {
+  if (Buffer.isBuffer(input)) return input
+  if (typeof input === 'undefined' || input === null) return input
+  return Buffer.from(input, 'hex')
+}
---
+++ test.js
@@ -0,0 +1,101 @@
+var test = require('tape')
+var multifeed = require('multifeed')
+var ram = require('random-access-memory')
+var hypercore = require('hypercore')
+var crypto = require('hypercore-crypto')
+var sigrid = require('.')
+
+
+test('Signature store serialization', function(t) {
+  t.plan(4)
+  var pair = crypto.keyPair()
+  var sig = sigrid(pair.publicKey, ram, pair.secretKey)
+  sig.setSignature('foo', 'bar', function(err) {
+      t.error(err)
+      t.equal(sig._signatures['foo'], 'bar', 'New signature visible after set')
+      delete sig._signatures // simulate loss of in-memory sighash
+      sig._reload(function(err) {
+        t.error(err)
+        t.equal(sig._signatures['foo'], 'bar', 'Signatures loaded correctly')
+      })
+  })
+})
+
+test('Signed replication', function(t) {
+  t.plan(23)
+  // Given a preshared keypair that can be derived
+  // from a secret phrase or generated and then
+  // shared.
+  var pair = crypto.keyPair()
+  // All three use the same public key, but only two of them know the private key.
+  var computer = null
+  var laptop = null
+  var hashbase = null
+  var unsignedFeed = null
+
+  function spawnMultiFeed(opts, cb) {
+    var haveSecret = !!opts.secretKey
+    var m = multifeed(hypercore, ram)
+    var sig = sigrid(opts.key, ram, opts.secretKey)
+    m.use(sig)
+    var buf = "dummy"
+
+    m.once('feed', function (feed, name) {
+      if (haveSecret) {
+        let signature = sig._signatures[feed.key.toString('hex')]
+        t.ok(signature, 'Should have a signature')
+        t.ok(crypto.verify(feed.key, Buffer.from(signature, 'hex'), m.key), 'signature should be verified')
+      } else {
+        // Should still produce the feed, but it will not be replicated anywhere.
+        t.equal(feed.key.length, 32)
+        unsignedFeed = feed.key.toString('hex')
+      }
+    })
+
+    m.writer(function (err, w) {
+      t.error(err)
+      t.ok(w.secretKey, 'Should be writeable regardless of signed state')
+      w.append(buf, function (err) {
+        t.error(err)
+        w.get(0, function (err, data) {
+          t.error(err)
+          t.equals(data.toString('utf8'), buf)
+          cb()
+        })
+      })
+    })
+    // t.deepEquals(m.feeds(), [w])
+    return m
+  }
+
+  function replicate(a, b, cb) {
+    var r = a.replicate()
+    r.pipe(b.replicate()).pipe(r)
+      .once('end', cb)
+  }
+
+  function fkeys(a) {
+    return a.map(function(f) { return f.key.toString('hex') }).sort()
+  }
+
+  computer = spawnMultiFeed({restricted: true, key: pair.publicKey, secretKey: pair.secretKey }, function(){
+    laptop = spawnMultiFeed({restricted: true, key: pair.publicKey, secretKey: pair.secretKey }, function(){
+      hashbase = spawnMultiFeed({restricted: true, key: pair.publicKey },function () {
+
+        var unsignedFeed = fkeys(hashbase.feeds())[0]
+        var signedFeeds = fkeys([].concat(computer.feeds()).concat(laptop.feeds()))
+        var allFeeds = [unsignedFeed].concat(signedFeeds).sort()
+
+        replicate(computer, laptop, function() {
+          replicate(hashbase, laptop, function() {
+            t.deepEquals(fkeys(computer.feeds()), signedFeeds)
+            t.deepEquals(fkeys(laptop.feeds()), signedFeeds)
+            t.deepEquals(fkeys(hashbase.feeds()), allFeeds)
+          })
+        })
+
+      })
+    })
+  })
+
+})
